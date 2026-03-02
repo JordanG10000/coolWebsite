ffab12e59f37d7c8d25718ccf6bdf853d7e112b9
JordanG10000 <jordangoodall001@gmail.com>
2026-03-02 18:28:44 +0000

    Mon, Mar 2, 2026, 10:28 AM -08:00

--- expandingPage/expanding.js
+++ expandingPage/expanding.js
@@ -2,7 +2,9 @@ const panels = querySelector(".panel");
 
 panels.addEventListener("click", (event) => {
     for (panel of panels) {
-        panel.classList.remove("selected");
+        panel.classList.remove("selected")
+        panel.classList.add("unselected");
     }
-
+    event.target.classlist.remove("unselected");
+    event.target.classList.add("selected");
 })
