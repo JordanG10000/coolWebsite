e99bfb64e290ce342938ccc5be29547ef575a382
JordanG10000 <jordangoodall001@gmail.com>
2026-03-03 18:19:23 +0000

    Tue, Mar 3, 2026, 10:19 AM -08:00

--- eventPage/event.js
+++ eventPage/event.js
@@ -1,8 +1,10 @@
 const clickBox = document.querySelector(".click");
 const spinBox = document.querySelector(".spin");
 
+console.log(Math.floor(Math.random(100,255)*100));
+
 clickBox.addEventListener("click", (e) => {
-    e.target.style.background = `rgb(${Math.random(100,255)*100}, ${Math.random(100,255)*100}, ${Math.random(100,255)*100})`
+    e.target.style.background = `rgb(${Math.floor(Math.random(100,255)*100)}, ${Math.random(100,255)*100}, ${Math.random(100,255)*100})`
 })
 
 spinBox.addEventListener("click", (e) => {
