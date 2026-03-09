08079f093ed4208129fbd7aa0717be60b50e5f10
JordanG10000 <jordangoodall001@gmail.com>
2026-03-09 16:38:46 +0000

    Mon, Mar 9, 2026, 9:38 AM -07:00

---
+++ interactivewebPage/interactive.js
@@ -0,0 +1,9 @@
+// Random list of words and a random list of first and last names
+
+let names = ["Jake", "Isaiah", "]
+
+// take the list of words and get a random number to represnet how many words will be added into quote
+
+// take 2 names from name list and use as the author
+
+// run when user clicks the button
