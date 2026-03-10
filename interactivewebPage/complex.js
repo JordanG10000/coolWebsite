38ca7f1ec2f1c5cd2b9c1f868a1c7e5a93811492
JordanG10000 <jordangoodall001@gmail.com>
2026-03-10 23:38:27 +0000

    Tue, Mar 10, 2026, 4:38 PM -07:00

---
+++ interactivewebPage/complex.js
@@ -0,0 +1,66 @@
+const lengthIndicator = document.querySelector("h2");
+const quoteText = document.querySelector(".quote");
+const authorText = document.querySelector(".author");
+const generateBtn = document.querySelector(".generateBtn");
+const trigger = document.querySelector(".switch");
+const slider = document.querySelector(".slider");
+
+// Random list of words and a random list of first and last names
+
+trigger.addEventListener("click", () => {
+    slider.classList.toggle("complex");
+})
+
+let names = ["Jake", "Isaiah", "James", "John", "Bill", "Faridoni", "Jordan", "Huesca", "Austin", "Bowling", "Williams", "Goodall"];
+let words = ["the", "was", "is", "am", "when", "how", "because", "I", "Jake Faridoni"];
+let nouns = [];
+let verbs = [];
+
+let punctuation = [".", "!", "?", "...",];
+
+generateBtn.addEventListener("click", () => {
+    if (slider.classList.contains("complex")) {
+        complexQuote();
+    }
+    else {
+        simpleQuote();
+    }
+    getAuthor();
+})
+
+function complexQuote() {
+
+}
+
+function simpleQuote() {
+    let text = "";
+    let quoteLength = (Math.floor(Math.random() * words.length));
+    for (let i = 0; i < quoteLength; i++)
+    {
+        // Gets as many words as the length of the quote
+        let randomWord = `${words[Math.floor(Math.random() * words.length)]}`
+        // ensures there isnt a space at the start of the quote, otherwise adds spaces
+        if (i == 0) {
+            text += randomWord;
+        }
+        else {
+            text += ` ${randomWord}`;
+        }
+    }
+    lengthIndicator.innerText = `Quote Length: ${quoteLength}`;
+    let rndmPunctuation = punctuation[Math.floor(Math.random() * punctuation.length)];
+    quoteText.innerText = text + rndmPunctuation;
+
+
+}
+
+function getAuthor() {
+     let author = "-";
+    // grabs 2 names for the author from the name list
+    for (let i = 0; i < 2; i++) {
+        let randomName = names[Math.floor(Math.random() * names.length)];
+        author += ` ${randomName}`;
+    }
+    // Places generated text into the html to display it to the user.
+    authorText.innerText = author;
+}
