5a968d172914d4372f0f79fffd638e9a70265dee
JordanG10000 <jordangoodall001@gmail.com>
2026-03-11 16:32:35 +0000

    Wed, Mar 11, 2026, 9:32 AM -07:00

--- soundPage/sound.js
+++ soundPage/sound.js
@@ -1,6 +1,7 @@
 const buttons = document.querySelector(".buttons");
+const sounds = document.querySelector("audio");
 
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i < sounds.length; i++) {
     btn = document.createElement("button");
     btn.classList.add("btn");
     btn.innerText = "Click Me";
@@ -8,7 +9,7 @@ for (let i = 0; i < 5; i++) {
     btn.addEventListener("click", () => {
         btn.classList.toggle("active");
     })
-    
+
     buttons.appendChild(btn);
 }
 
