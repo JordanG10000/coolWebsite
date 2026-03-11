c2c7a211f6dd0849cf2e2cfd65fd41927446bb4e
JordanG10000 <jordangoodall001@gmail.com>
2026-03-11 17:13:28 +0000

    Wed, Mar 11, 2026, 10:13 AM -07:00

--- soundPage/sound.js
+++ soundPage/sound.js
@@ -1,26 +1,30 @@
 const buttons = document.querySelector(".buttons");
 const sounds = document.querySelectorAll("audio");
 
-        setInterval(() => {
-            btn.classList.toggle("active")
-        }, sound.duration * 1000)
+
 
 for (let sound of sounds) {
     btn = document.createElement("button");
     btn.classList.add("btn");
     btn.innerText = `${sound.id.toUpperCase()}`;
 
+
+
     btn.addEventListener("click", () => {
+        let togge = setInterval(() => {
+            btn.classList.toggle("active")
+        }, sound.duration * 1000)
+
         if (!sound.paused) {
             btn.classList.toggle("active");
             sound.pause()
             sound.currentTime = 0;
-            clearInterval()
         }
         else {
             sound.play();
             btn.classList.toggle("active")
         }
+        clearInterval(togge);
     })
 
     buttons.appendChild(btn);
