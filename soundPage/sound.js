b300a4add99bb8b5658ea771e017a3bc5ec6a90c
JordanG10000 <jordangoodall001@gmail.com>
2026-03-11 16:16:29 +0000

    Wed, Mar 11, 2026, 9:16 AM -07:00

--- soundPage/sound.js
+++ soundPage/sound.js
@@ -4,6 +4,9 @@ for (let i = 0; i < 1000; i++) {
     btn = document.createElement("button");
     btn.classList.add("btn");
     btn.innerText = "Click Me";
+    btn.addEventListener("click", () => {
+        btn.classList.toggle("active");
+    })
     buttons.appendChild(btn);
 }
 
