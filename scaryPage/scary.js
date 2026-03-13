6f6db75192983ae10d32feb8c9840ef76d779a4f
JordanG10000 <jordangoodall001@gmail.com>
2026-03-13 16:55:01 +0000

    Fri, Mar 13, 2026, 9:55 AM -07:00

---
+++ scaryPage/scary.js
@@ -0,0 +1,12 @@
+const body = document.querySelector("body");
+
+
+function createMessage() {
+    let message = document.createElement("h1");
+    message.innerText = "WATCH OUT";
+    message.style.backgroundColor = "red";
+    body.append(message);
+    window.open("scary.html");
+}
+
+setInterval(createMessage(), 5000)
