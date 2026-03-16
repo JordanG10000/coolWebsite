31b928cd3d73e874d0473687f5e282645d681f7b
JordanG10000 <jordangoodall001@gmail.com>
2026-03-16 10:12:23 -0700

    finished welcome page

--- scaryPage/scary.js
+++ scaryPage/scary.js
@@ -3,10 +3,10 @@ const body = document.querySelector("body");
 
 function createMessage() {
     let message = document.createElement("h1");
-    message.innerText = "WATCH OUT";
+    message.innerText = "SELF-DESTRUCTING";
     message.style.backgroundColor = "red";
     body.append(message);
     window.open("scary.html");
 }
 
-setInterval(createMessage(), 50000)
+setInterval(createMessage(), 1000)
