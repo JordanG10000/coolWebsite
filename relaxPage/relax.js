764546be33a7015147267f5e1608832edc656c20
JordanG10000 <jordangoodall001@gmail.com>
2026-04-08 16:13:22 +0000

    Wed, Apr 8, 2026, 9:13 AM -07:00

--- relaxPage/relax.js
+++ relaxPage/relax.js
@@ -2,4 +2,5 @@ const container = document.querySelector(".container");
 const text = document.querySelector(".text");
 
 let totalTime = 7500;
-let breathTime = (totalTime / 5)
+let breathTime = (totalTime / 5) * 2;
+let holdTime = (totalTime / 5)
