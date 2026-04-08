9c123f113081083dafd07f584391ed16cf68b098
JordanG10000 <jordangoodall001@gmail.com>
2026-04-08 16:20:45 +0000

    Wed, Apr 8, 2026, 9:20 AM -07:00

--- relaxPage/relax.js
+++ relaxPage/relax.js
@@ -4,3 +4,14 @@ const text = document.querySelector(".text");
 let totalTime = 7500;
 let breathTime = (totalTime / 5) * 2;
 let holdTime = (totalTime / 5)
+
+breathAnim()
+
+function breathAnim() {
+    text.innerText = 'Breath in!':
+    container.classList.add('Grow');
+
+    setTimeout( ()=> {
+        text.innerText = 'Hold';
+    }, breathTime);
+}
