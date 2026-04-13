ac7daa396fa30a0a4a4466ccb6a792546c64d093
JordanG10000 <jordangoodall001@gmail.com>
2026-04-13 17:10:10 +0000

    Mon, Apr 13, 2026, 10:10 AM -07:00

---
+++ ballPage/ball.js
@@ -0,0 +1,103 @@
+
+// Window Variables
+let windowHeight = window.innerHeight;
+let windowWidth = window.innerWidth;
+
+// Ball Variables
+const ball = document.createElement("div");
+let ballRadius = 30;
+let xPos = windowWidth / 2 - ballRadius;
+let yPos = windowHeight / 2 - ballRadius;
+let xDirection = 1;
+let yDirection = 1;
+let speed = 5;
+let s = ball.style
+
+// Paddel Variables
+const lPaddel = document.createElement("div");
+document.body.appendChild(lPaddel);
+let padWidth = 20;
+let padHeight = 150;
+let pPosY = windowHeight / 2 - padHeight / 2;
+let pUp = false;
+let pDown = false;
+let padSpeed = 10;
+let p = lPaddel.style;
+
+
+createLPaddel();
+createBall();
+
+
+setInterval(moveBall, 10);
+
+function moveBall() {
+    xPos += xDirection * speed;
+    yPos += yDirection * speed;
+    s.left = `${xPos}px`;
+    s.top = `${yPos}px`;
+    if (yPos > (windowHeight - (ballRadius * 2)) || yPos < 0) {
+        yDirection *= -1;
+    }
+    if (xPos > (windowWidth - (ballRadius * 2)) || xPos < 0) {
+        xDirection *= -1;
+    }
+    if (pUp === true) {
+        pPosY -= padSpeed;
+    }
+    if (pDown === true) {
+        pPosY += padSpeed;
+    }
+}
+
+function createLPaddel() {
+    p.height = `${padHeight}px`;
+    p.width = `${padWidth}px`;
+    p.backgroundColor = 'blue';
+    p.position = 'absolute';
+    p.left = `50px`;
+    p.top = `${windowHeight / 2 - padHeight / 2}px`;
+}
+
+function createBall() {
+    document.body.appendChild(ball);
+    s.height = `${2 * ballRadius}px`;
+    s.width = `${2 * ballRadius}px`;
+    s.borderRadius = '50%';
+    s.backgroundColor = 'red';
+    s.position = 'absolute';
+    s.top = `${windowHeight / 2 - ballRadius}px`;
+    s.left = `${windowWidth / 2 - ballRadius}px`;
+}
+
+window.addEventListener('resize', () => {
+    windowHeight = window.innerHeight;
+    windowWidth = window.innerWidth;
+})
+
+document.addEventListener('keydown', (e) => {
+    console.log(e.key);
+    if (e.key == 'w' || e.key == 'ArrowUp') {
+        if (pPosY >= 0) {
+            pUp = true;
+            pPosY -= padSpeed;
+        }
+    }
+    if (e.key == 's' || e.key == 'ArrowDown') {
+        if (pPosY <= windowHeight - padHeight) {
+            pDown = true;
+            pPosY += padSpeed;
+        }
+    }
+    p.top = `${pPosY}px`;
+})
+
+document.addEventListener('keyup', (e) => {
+    console.log(e.key);
+    if (e.key == 'w' || e.key == 'ArrowUp') {
+        pUp = false;
+    }
+    if (e.key == 's' || e.key == 'ArrowDown') {
+        pDown = true;
+    }
+})
